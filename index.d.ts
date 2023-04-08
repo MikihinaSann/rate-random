fd17836f3f441d95e9100fa2d07fac27c3ab6c6f
ElainaSann <mikihinasann@gmail.com>
2023-04-08 14:29:02 +0700

    update type obj

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,5 @@
-export function rate(obj: object): number;
+export function rate(obj: Dictionary): number;
 
-export function randomArr(obj: object, string: string): any;
+export function randomArr(obj: Dictionary, string: string): any;
+
+export type Dictionary<V = any, K extends string | symbol = string> = Record<K, V>;
